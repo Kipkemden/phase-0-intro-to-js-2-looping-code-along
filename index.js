d4b54fb3a24e582f2d2b0c57f4bbde42751fc2bc
Cheruiyot Kipkemoi <d.cheru112@gmail.com>
2023-03-27 00:17:10 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(names, giftType) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${giftType} gift!`;
+      messages.push(message);
+    }
+    return messages;
+  }
+  const names = ["Guadalupe", "Ollie", "Aki"];
+  const giftType = "surprise";
+  const messages = writeCards(names, giftType);
+  console.log(messages);
+
+  function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+      console.log(i);
+    }
+  }
